bf8daaa8d93c47b0a1a3c48b3e9222244b2612ce
Krzysztof Sobkowiak <krzysztof_91@hotmail.com>
2021-01-31 12:08:29 +0100

    Exceptions

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -0,0 +1,45 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightSearch{
+    public void findFlight(Flight flight) throws RouteNotFoundException{
+
+        Map<String,Boolean> listOfFlight = new HashMap<>();
+        listOfFlight.put("Poznań",true);
+        listOfFlight.put("Argentina",false);
+
+        Boolean departureAirport = listOfFlight.get(flight.getDepartureAirport());
+        Boolean arrivalAirport = listOfFlight.get(flight.getArrivalAirport());
+
+        if(departureAirport == null || arrivalAirport == null){
+           throw new RouteNotFoundException("Sorry, flight from - " + flight.getDepartureAirport() + " - to - " + flight.getArrivalAirport() +
+                   " The selected flight doesn`t exist, it`s not on the our list.");
+        } else {
+            System.out.println("The selected flight is on the our list, departing from - " +
+                    flight.getDepartureAirport() + " - to - " + flight.getArrivalAirport());
+        }
+    }
+    public static void main(String[] args){
+        FlightSearch flightFinder = new FlightSearch();
+
+        try {
+            System.out.println("Processing Flight Search");
+            flightFinder.findFlight(new Flight("Poznań", "London"));
+        } catch (RouteNotFoundException e){
+            System.out.println("Error # " + e);
+        } finally {
+            System.out.println("Process flight search was done");
+        }
+
+        try {
+            System.out.println("\nProcessing Flight Search");
+            flightFinder.findFlight(new Flight("Poznań", "Argentina"));
+        } catch (RouteNotFoundException e){
+            System.out.println("Error # " + e);
+        } finally {
+            System.out.println("Process flight search was done");
+        }
+    }
+}
