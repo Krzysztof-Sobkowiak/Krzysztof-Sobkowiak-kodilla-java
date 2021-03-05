0690fe3ee7d70dad50023dcda7626c1ba95a984e
Krzysztof Sobkowiak <krzysztof_91@hotmail.com>
2021-03-05 17:09:47 +0100

    Revert "Order Service"
    This reverts commit b07f07b1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/service/DeliveryService.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.good.patterns.challenges.service;
-
-import com.kodilla.good.patterns.challenges.user.User;
-
-public interface DeliveryService {
-
-    boolean deliveryTo(final User user, boolean sendMessage, boolean paracel, boolean post, boolean courier);
-
-}
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.good.patterns.challenges.service;
-
-import com.kodilla.good.patterns.challenges.order.Order;
-
-public interface PaymentService {
-
-    boolean payment(final Order order, boolean paid);
-
-}
